bc5d4fff13146b164f15e9a94e7864d508852627
Dean McHugh <dean.mchugh1988@gmail.com>
2019-02-27 10:52:21 +1000

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <p>navbar</p>
-      </div>
+      <React.Fragment>
+        <Link to='/' alt='home'>Home</Link>
+        <Link to='/project' alt='project'>Project</Link>
+        <Link to='/work' alt='work'>Work</Link>
+        <Link to='/blog' alt='blog'>Blog</Link>
+      </React.Fragment>
     )
   }
 }
