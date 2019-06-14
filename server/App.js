1f2d6ea0661ed887562f553ea69fef0e556ba87d
Dean McHugh <dean.mchugh1988@gmail.com>
2019-06-14 19:34:45 +1000

    dependencies

--- server/App.js
+++ server/App.js
@@ -1,10 +1,21 @@
+const bodyParser = require('body-parser')
 const express = require('express')
+const helmet = require('helmet')
 const https = require('https')
 const http = require('http')
+const cors = require('cors')
 const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(helmet())
+app.use(cors())
 
 app.get('/', (rep, res) => {
     res.send('hello back end!')
 })
 
-http.createServer(app).listen(5000)
+const port = process.env.PORT || 5000
+http.createServer(app).listen(port, () => {
+    console.log('Server is running on port ' + port)
+})
