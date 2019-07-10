a9992e95a0acc532fc8bcf9d5901ce25da3c9669
Dean McHugh <dean.mchugh1988@gmail.com>
2019-07-10 19:44:56 +1000

    dynamic url

--- client/src/components/portfolio/model/PortfolioPost.js
+++ client/src/components/portfolio/model/PortfolioPost.js
@@ -10,7 +10,7 @@ export default class ProjectPost extends Component {
             <h3 className='project-language' style={{display: !this.props.language && "none"}}>{this.props.language}</h3>
             <p className='project-description-show' style={{display: !this.props.description && "none"}}>{this.props.description}</p>
             <a className='project-repository' style={{display: !this.props.repository && "none"}} href={this.props.repository} alt='git repository' target='blank'>Git Repository</a>
-            <a className='project-url' style={{display: !this.props.url && "none"}} href={this.props.url} alt='project url' target='blank'>lisagordonart.com</a>
+            <a className='project-url' style={{display: !this.props.url && "none"}} href={this.props.url} alt='project url' target='blank'>{this.props.url}</a>
           </div>
       </div>
     )
