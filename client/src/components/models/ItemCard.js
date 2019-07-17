b18f0404add7d3310ebc2a0c8db3e045b031adef
Dean McHugh <dean.mchugh1988@gmail.com>
2019-07-17 19:09:30 +1000

    sizing

--- client/src/components/models/ItemCard.js
+++ client/src/components/models/ItemCard.js
@@ -26,8 +26,8 @@ export default class StudyPost extends Component {
                     <h3 className='card-title-2' style={{display: !this.props.language && "none"}}>{this.props.language}</h3>
                     <h3 className='card-title-2' style={{display: !this.props.company && "none"}}>{this.props.company}</h3>
                     <h4 className='card-title-3' style={{display: !this.props.timePeriod && "none"}}>{this.props.timePeriod}</h4>
-                    <a style={{display: !this.props.repository && "none"}} href={this.props.repository} alt='git repository' target='blank'>Git Repository</a>
                     <div><a style={{display: !this.props.url && "none"}} href={this.props.url} alt='project url' target='blank'>Project Website</a></div>
+                    <a style={{display: !this.props.repository && "none"}} href={this.props.repository} alt='git repository' target='blank'>Git Repository</a>
                 </div>
                 <p className='card-skills' style={{display: !this.props.skills && "none"}}>Skills: {this.props.skills}</p>
             </div>
